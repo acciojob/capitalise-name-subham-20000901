25595bf51a05a853b34f50f121eb02e552e153a5
subham-20000901 <sinhasubh2000@gmail.com>
2025-12-23 23:45:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+function upperCase(str){
+  str = str.toUpperCase();
+	return str;
+	
+}
+// Please do not change the code below
+ const s1 = prompt("Enter s1:");
+alert(upperCase(s1));
